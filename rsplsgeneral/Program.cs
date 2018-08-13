90ad5bdc11ca0252997d572f113e90e2b6cec2af
Lizaveta <elis.tkachuk@gmail.com>
2018-08-13 15:22:13 +0300

    ready project

---
+++ rsplsgeneral/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace rsplsgeneral
+{
+    public abstract class ComputerChoice
+    {
+        protected const int n = 7;
+        protected static string[] move = new string[n] { "Rock", "Scissors", "Paper", "Lizard", "Spock","Pencil" ,"Fire"};
+        protected int comp;
+        private int key;
+        private byte[] computer = new byte[1];
+        public void Computermove()
+        {           
+            RNGCryptoServiceProvider Generate = new RNGCryptoServiceProvider();
+            Generate.GetBytes(computer);
+            comp = Convert.ToInt32(computer[0]) % n;
+            return;
+        }
+        public void GettheKey()
+        {
+            byte[] keybyte = new byte[1];
+            RNGCryptoServiceProvider Gen = new RNGCryptoServiceProvider();
+            Gen.GetBytes(keybyte);
+            key = Convert.ToInt32(keybyte[0]);
+        }
+        public int GettheHash()
+        {
+            HashAlgorithm sha = new SHA1CryptoServiceProvider();
+            byte[] hash = sha.ComputeHash(computer);
+            int hashint = Convert.ToInt32(hash[0]) + key;
+            Console.WriteLine("The computer move's hash is {0}", hashint);
+            return hashint;
+        }
+        public void Showthemove()
+        {
+            Console.WriteLine("the computer has chosen {0}", move[comp]);
+        }
+        public void Showthekey()
+        {
+            Console.WriteLine("the hash key is {0}", key);
+        }
+    };
+    public  class User : ComputerChoice
+    {
+
+        protected int user;
+        protected bool t;
+        public User() { user = 0;t = false; }        
+        public void UserChoice()
+        {
+            Console.WriteLine("Choose your move ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(i + " - " + move[i]);
+            }
+            user = Convert.ToInt32(Console.ReadLine());
+            if(user>=n)
+            {
+                Exceptions Obj = new Exceptions("The number you've entered is out of range, try again");
+                throw Obj;
+            }
+        }
+        public void Compare()
+        {
+            if (comp == user)
+            {
+                Exceptions Obj = new Exceptions(move[comp] + " vs " + move[user] + "= try again");
+                throw Obj;
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (comp == j && user == i) { Console.WriteLine(move[comp] + " crushes " + move[user]); t = false; }
+                        if (comp == i && user == j) { Console.WriteLine(move[user] + " crushes " + move[comp]); t = true; }
+                    }
+                    else
+                    {
+                        if (comp == j && user == i) { Console.WriteLine(move[user] + " crushes " + move[comp]); t = true; }
+                        if (comp == i && user == j) { Console.WriteLine(move[comp] + " crushes " + move[user]); t = false; }
+                    }
+                }
+            }
+            if (t == true) Console.WriteLine(" You've won!!!");
+            else if (t == false) Console.WriteLine("You've lost!!!");
+
+        }
+
+    };
+    public class Exceptions: Exception
+    {
+        private string Message;
+        public Exceptions(string a) { Message = a; }
+        public void Showthemessage() { Console.WriteLine(Message); }
+        
+    };
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+            User A = new User();
+            A.Computermove();
+            A.GettheKey();
+            A.GettheHash();
+            A.UserChoice();
+            A.Showthekey();
+            A.Showthemove();
+            A.Compare();
+            Console.ReadLine();
+            }
+        catch (Exceptions obj )
+            {
+                obj.Showthemessage();
+                Console.ReadLine();
+            }
+            
+        }
+
+    }
+}
---
+++ rsplsgeneral/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("rsplsgeneral")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rsplsgeneral")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("252a08c3-6c9f-41c3-8474-00a9edcd1f5e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
